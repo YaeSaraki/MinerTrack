9cf0c581f7c6c24020f15b8cf6538dbf85fadb15
At87668 <author87668@hotmail.com>
2024-12-07 18:08:46 +0800

    1.6.0-beta dev

--- src/main/java/link/star_dust/MinerTrack/managers/LanguageManager.java
+++ src/main/java/link/star_dust/MinerTrack/managers/LanguageManager.java
@@ -1,3 +1,14 @@
+/**
+ * DON'T REMOVE THIS
+ * 
+ * /MinerTrack/src/main/java/link/star_dust/MinerTrack/managers/LanguageManager.java
+ * 
+ * MinerTrack Source Code - Public under GPLv3 license
+ * Original Author: Author87668
+ * Contributors: Author87668
+ * 
+ * DON'T REMOVE THIS
+**/
 package link.star_dust.MinerTrack.managers;
 
 import org.bukkit.ChatColor;
